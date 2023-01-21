cd8865241332758af940e87da398750e76afddc4
Eren Düzova <erenduzova@gmail.com>
2023-01-21 16:47:49 +0300

    Initial Commit

---
+++ src/main/java/com/erenduzova/ticketary/TicketaryApplication.java
@@ -0,0 +1,13 @@
+package com.erenduzova.ticketary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erenduzova/ticketary/TicketaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erenduzova.ticketary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
