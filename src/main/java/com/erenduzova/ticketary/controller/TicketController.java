002111177bb0e4e6752557c64bb1f94c20224e49
Eren Düzova <erenduzova@gmail.com>
2023-01-28 23:42:55 +0300

    Add: TicketController Endpoints Added For Get All Tickets And Create Tickets

--- src/main/java/com/erenduzova/ticketary/controller/TicketController.java
+++ src/main/java/com/erenduzova/ticketary/controller/TicketController.java
@@ -1,9 +1,13 @@
 package com.erenduzova.ticketary.controller;
 
+import com.erenduzova.ticketary.dto.model.request.PassengerRequest;
+import com.erenduzova.ticketary.dto.model.response.TicketResponse;
 import com.erenduzova.ticketary.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/tickets")
@@ -12,6 +16,19 @@ public class TicketController {
     @Autowired
     private TicketService ticketService;
 
+    // Get All Tickets
+    @GetMapping
+    public ResponseEntity<List<TicketResponse>> getAll() {
+        return ResponseEntity.ok(ticketService.getAll());
+    }
+
+    // Create Tickets
+    @PostMapping(value = "/{buyerId}/{travelId}")
+    public ResponseEntity<List<TicketResponse>> createTickets(@PathVariable Long buyerId,
+                                                              @PathVariable Long travelId,
+                                                              @RequestBody List<PassengerRequest> passengerRequestList) {
+        return ResponseEntity.ok(ticketService.createTickets(buyerId, travelId, passengerRequestList));
+    }
 
 
 }
