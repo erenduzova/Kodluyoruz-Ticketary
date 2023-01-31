315ac44884ac62fe222974eef298ff7f35855de0
Eren Düzova <erenduzova@gmail.com>
2023-01-31 05:44:03 +0300

    New microservice created for payments and accounts(ticketary-payment-service)
    This service uses port 8081 and controls, saves payments and accounts data.

---
+++ ticketary-payment-service/src/main/java/com/erenduzova/ticketarypaymentservice/TicketaryPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erenduzova.ticketarypaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketaryPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketaryPaymentServiceApplication.class, args);
+	}
+
+}
